c9f08dee008d6532b3a6cbe4f9af8da53a8d12c1
Muhammad Hafif Al Busyro <41612593+hafif07@users.noreply.github.com>
2019-12-05 23:24:03 +0700

    change content text in content 1

--- src/Component/Home/Content1/Content1.jsx
+++ src/Component/Home/Content1/Content1.jsx
@@ -5,13 +5,13 @@ const Content1 = () => {
   return (
     <>
       <div className="box-our">
-        <p className="text-center title-desc">Our Vision</p>
+        <p className="text-center title-desc">Visi Supercamp Programmer</p>
         <div className="row box-content1">
           <div className="col-md-12 box-title-content1">
             <div className="card card-box-content1">
               <p className="text-center">
-                <i className="fa fa-quote-left"></i> Menjadi lembaga pendidikan
-                yang menyiapkan programmer profesional , kreatif ,serta inovatif
+                <i className="fa fa-quote-left"></i> Menjadi wadah pendidikan
+                yang menyiapkan programmer profesional, kreatif, serta inovatif
                 yang mampu bersaing di dunia nasional dan internasional{" "}
                 <i className="fa fa-quote-right"></i>
               </p>
