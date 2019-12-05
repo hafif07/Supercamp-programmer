036ebb78bcde2f7795f1b7864a04a4dc75cc8ff1
Dimas1402 <anugerahdimas469@gmail.com>
2019-12-05 16:13:11 +0700

    Dimas lokasi

--- src/Component/Pendaftaran/Pendaftaran.jsx
+++ src/Component/Pendaftaran/Pendaftaran.jsx
@@ -5,9 +5,13 @@ import ContentPendaftaran1 from "./Content1/ContentPendaftaran1";
 import CollapsePendaftaran from "./Content2/CollapsePendafataran";
 import Footer from "../Home/Footer/Footer";
 import TablePendaftaran from "../Pendaftaran/Content2/TablePendaftaran";
+import Copyright from "../Copyright/Copyright";
+import NavbarProgram from "../Program3hari/Navbar/Navbar3hari";
 
 const Pendaftaran = () => {
   return (
+    <>
+    <NavbarProgram/>
     <div className="container-fluid pendaftaran">
       <div className="row">
         <div className="col-md-12 bg-pendaftaran">
@@ -26,8 +30,12 @@ const Pendaftaran = () => {
         <div className="col-md-12 footer">
           <Footer />
         </div>
+        <div className="col-md-12">
+          <Copyright/>
+        </div>
       </div>
     </div>
+    </>
   );
 };
 
